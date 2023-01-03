176d7a0fbccc5fc0edec78742c580b4eac8d655c
antdono <adon.uiux@gmail.com>
2023-01-03 14:39:29 -0600

    did my first assignments

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,4 +1,27 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
 
-console.log("Jonass times 2");
+// console.log("Jonas");
+// console.log(23);
+
+// let firstName = "Matilda";
+
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
+
+// let jonas_matilda = "JM";
+// let $function = 27;
+
+// let person = "jonas";
+// let PI = 3.1415;
+
+// discritive variable names
+
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+
+// let job1 = "Programmer";
+// let job2 = "Teacher";
+
+// console.log(myFirstJob);
