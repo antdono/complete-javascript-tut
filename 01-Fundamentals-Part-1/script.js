1e76114b3dbea1a6bfe879273e9a66c3bdf88e67
antdono <adon.uiux@gmail.com>
2023-02-01 15:08:13 -0600

    working on basic assignment operators, will be working on operator precedence next.

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,27 +1,97 @@
-// let js = "amazing";
-// console.log(40 + 8 + 23 - 10);
+/*
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
 
-// console.log("Jonas");
-// console.log(23);
+console.log("Jonas");
+console.log(23);
 
-// let firstName = "Matilda";
+let firstName = "Matilda";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
 
-// console.log(firstName);
-// console.log(firstName);
-// console.log(firstName);
+//* Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+let person = "jonas";
+let PI = 3.1415;
 
-// let jonas_matilda = "JM";
-// let $function = 27;
+//* discritive variable names
 
-// let person = "jonas";
-// let PI = 3.1415;
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+let job1 = "Programmer";
+let job2 = "Teacher";
+console.log(myFirstJob);
+*/
 
-// discritive variable names
+//* Data types
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
 
-// let myFirstJob = "Programmer";
-// let myCurrentJob = "Teacher";
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "tony");
 
-// let job1 = "Programmer";
-// let job2 = "Teacher";
+javascriptIsFun = "YES!";
+console.log(typeof javascriptIsFun);
 
-// console.log(myFirstJob);
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+console.log(typeof null);
+*/
+/*
+//* let, const and var
+
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+
+var job = "programmer";
+job = "teacher";
+
+lastName = "Schmedtmann";
+console.log(lastName);
+*/
+
+//* Math operators
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+//* Assignment operators
+
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--; // x = x - 1
+console.log(x);
+
+//* Comparison operators
+
+console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(now - 1991 > -2018);
