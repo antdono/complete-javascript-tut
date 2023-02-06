3f2f807936139b6d71f0b48353eb41f7e801ff6a
antdono <adon.uiux@gmail.com>
2023-02-05 19:30:17 -0600

    learned about type conversion, type coercion, truthy falsy values, and equality operators.

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -66,32 +66,205 @@ console.log(lastName);
 
 //* Math operators
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
 
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
 
-const firstName = "Jonas";
-const lastName = "Schmedtmann";
-console.log(firstName + " " + lastName);
+// const firstName = "Jonas";
+// const lastName = "Schmedtmann";
+// console.log(firstName + " " + lastName);
 
-//* Assignment operators
+// //* Assignment operators
 
-let x = 10 + 5; // 15
-x += 10; // x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x++; // x = x + 1
-x--; // x = x - 1
-x--; // x = x - 1
-console.log(x);
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--; // x = x - 1
+// x--; // x = x - 1
+// console.log(x);
 
 //* Comparison operators
 
-console.log(ageJonas > ageSarah); // >, <, >=, <=
-console.log(ageSarah >= 18);
+// console.log(ageJonas > ageSarah); // >, <, >=, <=
+// console.log(ageSarah >= 18);
 
-const isFullAge = ageSarah >= 18;
-console.log(now - 1991 > -2018);
+// const isFullAge = ageSarah >= 18;
+// console.log(now - 1991 > -2018);
+
+// Operator precedence
+
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+
+// console.log(now - 1991 > -2018);
+// console.log(25 - 10 - 5);
+// console.log(5 - 10 - 25);
+
+// let x, y;
+// x = y = 25 - 10 - 5; // x = y = 10 , x = 10
+// console.log(x, y);
+
+// const averageAge = (ageJonas + ageSarah) / 2;
+// console.log(ageJonas, ageSarah);
+// console.log(averageAge);
+
+//! Coding Challenge #1
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+// const markMass = 95;
+// const markHeight = 1.88;
+// const johnMass = 85;
+// const johnHeight = 1.76;
+
+// const BMIMark = markMass / markHeight ** 2;
+// const BMIJohn = johnMass / (johnHeight * johnHeight);
+// const markHigherBMI = BMIMark > BMIJohn;
+
+// console.log(BMIMark, BMIJohn);
+// console.log(markHigherBMI);
+
+//! Template literals
+
+// const firstName = "Jonas";
+// const job = "teacher";
+// const birthYear = 1991;
+// const year = 2037;
+
+// const jonas =
+//   "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+// console.log(jonas);
+
+// const jonasNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+// console.log(jonasNew);
+
+// console.log(
+//   "string with \n\
+// multiple \n\
+// lines"
+// ); // old way and is actually a bug
+
+// console.log(`string
+// multiple
+// lines`);
+
+//! taking decision: if / else statements
+
+// const age = 19;
+
+// if (age >= 18) {
+//   console.log("Sarah can start driving license 🚗");
+// } else {
+//   const yearsLeft = 18 - age;
+//   console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+// }
+
+// const birthYear = 1991;
+
+// let century;
+// if (birthYear <= 2000) {
+//   century = 20;
+// } else {
+//   century = 21;
+// }
+// console.log(century);
+
+//! Coding Challenge #2
+
+// const markMass = 78;
+// const markHeight = 1.69;
+// const johnMass = 92;
+// const johnHeight = 1.95;
+
+// const markMass = 95;
+// const markHeight = 1.88;
+// const johnMass = 85;
+// const johnHeight = 1.76;
+
+// const BMIMark = markMass / markHeight ** 2;
+// const BMIJohn = johnMass / (johnHeight * johnHeight);
+// console.log(BMIMark, BMIJohn);
+
+// if (BMIMark > BMIJohn) {
+//   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+// } else {
+//   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+// }
+
+//! Type conversion
+
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number("Jonas")); // NaN
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// //! type coercion
+// console.log("I am " + 23 + " years old");
+// console.log("23" - "10" - 3);
+// console.log("23" * "2");
+
+// let n = "1" + 1; // '11'
+// n = n - 1; // 10
+// console.log(n); // 10
+
+//! Truthy and Falsy values
+
+//? 5 falsy values: 0, '', undefined, null, NaN
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean("Jonas"));
+// console.log(Boolean({}));
+// console.log(Boolean(""));
+
+// const money = 100;
+// if (money) {
+//   console.log("Don't spend it all ;)");
+// } else {
+//   console.log("You should get a job!");
+// }
+
+// let height = 123;
+// if (height) {
+//   console.log("YAY! Height is defined");
+// } else {
+//   console.log("Height is undefined");
+// }
+
+//! Equality operators: == vs. ===
+
+// const age = 18;
+
+// if (age === 18) console.log("You just became an adult :D (strict)");
+
+// if (age == 18) console.log("You just became an adult :D (loose)");
+
+// const favorite = Number(prompt("What is your favorite number?"));
+// console.log(typeof favorite);
+
+// if (favorite === 23) {
+//   // 23 == '23'
+//   console.log("Cool! 23 is an amazing number!");
+// } else if (favorite === 7) {
+//   console.log("7 is also a cool number");
+// } else if (favorite === 9) {
+//   console.log("9 is also a cool number");
+// } else {
+//   console.log("Number is not 23 or 7 or 9");
+// }
+
+// if (favorite !== 23) console.log("Why not 23?");
+
+//! boolean logic
